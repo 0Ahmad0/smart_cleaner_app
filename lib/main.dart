84f18c828f79e688f70b6ccfb6fea7d230472d27
Ahmad Alhariri <mr.ahmed.alhariri@gmail.com>
2024-09-25 17:34:00 +0300

    -add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_cleaner_app/firebase_options.dart';
 import 'package:smart_cleaner_app/smart_cleaner_app.dart';
-
 import 'core/routing/app_router.dart';
 import 'core/routing/routes.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   /// To Fix Bug In Text Showing In Release Mode
   await ScreenUtil.ensureScreenSize();
 
