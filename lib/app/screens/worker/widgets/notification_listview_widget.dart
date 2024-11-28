ad8516c4afa0824ad2d111c1706c4552a3155448
Ahmad Mriwed <mr.ahmadmriwed@gmail.com>
2024-11-28 14:38:44 +0300

    feat: add time in notification

--- lib/app/screens/worker/widgets/notification_listview_widget.dart
+++ lib/app/screens/worker/widgets/notification_listview_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:intl/intl.dart';
 import 'package:smart_cleaner_app/core/helpers/spacing.dart';
 import 'package:smart_cleaner_app/core/models/notification_model.dart';
 import 'package:smart_cleaner_app/core/utils/assets_manager.dart';
@@ -18,6 +19,7 @@ class NotificationsListviewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return ListView.separated(
       itemCount: list.length,
       separatorBuilder: (_, __) => Divider(
@@ -52,20 +54,33 @@ class NotificationsListviewWidget extends StatelessWidget {
         ),
         trailing:
 
-        Visibility(
-          visible: list[index].checkRec,
-          // visible: list.length.isEven,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-            decoration: BoxDecoration(
-                color: ColorManager.tealColor,
-                borderRadius:
-                    BorderRadius.horizontal(left: Radius.circular(100.r))),
-            child: Text(
-              'Seen',
-              style: StyleManager.font12Medium(color: ColorManager.whiteColor),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox.shrink(),
+            Visibility(
+              visible: list[index].checkRec,
+              // visible: list.length.isEven,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+                decoration: BoxDecoration(
+                    color: ColorManager.tealColor,
+                    borderRadius:
+                        BorderRadius.horizontal(left: Radius.circular(100.r))),
+                child: Text(
+                  'Seen',
+                  style: StyleManager.font12Medium(color: ColorManager.whiteColor),
+                ),
+              ),
             ),
-          ),
+            Text(
+              "${DateFormat("MMM d,").add_jm().format(list[index].dateTime??DateTime.now())}"+' ',
+              overflow: TextOverflow.ellipsis,
+              style: StyleManager.font12Regular(
+                  color: ColorManager.blackColor.withOpacity(.5)),
+            )
+          ],
         ),
       ),
     );
